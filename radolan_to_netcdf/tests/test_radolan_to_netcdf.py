681e6d0fae58a1e5489dac615e1f21673842cbfe
Christian Chwala <christian.chwala@kit.edu>
2019-03-06 23:39:52 +0100

    restructured test so that other products can be easily added

--- radolan_to_netcdf/tests/test_radolan_to_netcdf.py
+++ radolan_to_netcdf/tests/test_radolan_to_netcdf.py
@@ -12,35 +12,41 @@ def get_test_data_path():
     return pkg_resources.resource_filename('radolan_to_netcdf',
                                            'tests/test_data')
 
+def get_test_data_for_product(product_name):
+    fn_patterns = {
+        'RW': 'radolan_rw/raa01-rw_10000-181122*---bin.gz',
+    }
 
-class TestRadolanRW(unittest.TestCase):
-    def test_write_new_netcdf(self):
-        fn = 'test.nc'
-        radolan_to_netcdf.create_empty_netcdf(fn, product_name='RW')
+    return glob.glob(os.path.join(get_test_data_path(),
+                                  fn_patterns[product_name]))
 
-        data_list, metadata_list = [], []
+def parse_and_validate_test_data(product_name):
+    fn = 'test.nc'
+    radolan_to_netcdf.create_empty_netcdf(fn, product_name=product_name)
 
-        fn_radolan_files = glob.glob(
-            os.path.join(
-                get_test_data_path(),
-                'radolan_rw/raa01-rw_10000-181122*---bin.gz'))
+    data_list, metadata_list = [], []
 
-        for fn_radolan_file in fn_radolan_files:
-            data, metadata = radolan_to_netcdf.read_in_one_bin_file(
-                fn_radolan_file)
-            data_list.append(data)
-            metadata_list.append(metadata)
+    for fn_radolan_file in get_test_data_for_product(product_name):
+        data, metadata = radolan_to_netcdf.read_in_one_bin_file(
+            fn_radolan_file)
+        data_list.append(data)
+        metadata_list.append(metadata)
 
-        radolan_to_netcdf.create_empty_netcdf(fn=fn, product_name='RW')
+    radolan_to_netcdf.create_empty_netcdf(fn=fn, product_name=product_name)
 
-        radolan_to_netcdf.append_to_netcdf(
-            fn=fn,
-            data_list=data_list,
-            metadata_list=metadata_list)
+    radolan_to_netcdf.append_to_netcdf(
+        fn=fn,
+        data_list=data_list,
+        metadata_list=metadata_list)
 
-        with netCDF4.Dataset(fn, mode='r') as ds:
-            actual = ds['rainfall_amount'][:].filled(np.nan).sum(axis=0)
-            reference = np.stack(data_list, axis=2).sum(axis=2)
-            assert_almost_equal(actual, reference)
+    with netCDF4.Dataset(fn, mode='r') as ds:
+        actual = ds['rainfall_amount'][:].filled(np.nan).sum(axis=0)
+        reference = np.stack(data_list, axis=2).sum(axis=2)
+        assert_almost_equal(actual, reference)
 
-        os.remove(fn)
+    os.remove(fn)
+
+class TestWriteToFile(unittest.TestCase):
+    def test_RW(self):
+        parse_and_validate_test_data(product_name='RW')
+
