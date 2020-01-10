6dfe53df008bac7b32d7622ef9a9bfc866d5d07c
Christian Chwala <christian.chwala@kit.edu>
2020-01-10 11:16:07 +0100

    Added working implementation to parse wradlib metadata to header str
    Also had to fix the test function layout
    
    Also added code to make sure this is only used for RADOLAN-RW for now

--- radolan_to_netcdf/tests/test_write_to_radolan_bin.py
+++ radolan_to_netcdf/tests/test_write_to_radolan_bin.py
@@ -7,13 +7,25 @@ from radolan_to_netcdf import wradlib_to_radolan_bin
 from radolan_to_netcdf.tests.tools import get_test_data_for_product
 
 class TestWradlibMetadataToHeader(unittest.TestCase):
-    for fn_radolan_file in get_test_data_for_product('RW'):
-        data, metadata = radolan_to_netcdf.read_in_one_bin_file(
-            fn_radolan_file)
+    def test_RW(self):
+        for fn_radolan_file in get_test_data_for_product('RW'):
+            data, metadata = radolan_to_netcdf.read_in_one_bin_file(
+                fn_radolan_file)
 
-        with wrl.io.radolan.get_radolan_filehandle(fn_radolan_file) as f:
-            reference = wrl.io.radolan.read_radolan_header(f)
+            with wrl.io.radolan.get_radolan_filehandle(fn_radolan_file) as f:
+                reference = wrl.io.radolan.read_radolan_header(f)
 
-        actual = wradlib_to_radolan_bin.metadata_to_header(metadata)
+            actual = wradlib_to_radolan_bin.metadata_to_header(metadata)
 
-        assert actual == reference
+            assert actual == reference
+
+    def test_not_RW_error(self):
+        with self.assertRaises(NotImplementedError) as context:
+            fn_radolan_files = get_test_data_for_product('YW')
+            data, metadata = radolan_to_netcdf.read_in_one_bin_file(
+                fn_radolan_files[0])
+            wradlib_to_radolan_bin.metadata_to_header(metadata)
+
+        self.assertTrue(
+            'Currently only RADOALN-RW is supported' in str(context.exception)
+        )
--- radolan_to_netcdf/wradlib_to_radolan_bin.py
+++ radolan_to_netcdf/wradlib_to_radolan_bin.py
@@ -1,4 +1,5 @@
-import wradlib as wrl
+import datetime
+import numpy as np
 
 
 def metadata_to_header(metadata):
@@ -17,4 +18,71 @@ def metadata_to_header(metadata):
 
     """
 
-    return None
+    if metadata['producttype'] != 'RW':
+        raise NotImplementedError('Currently only RADOALN-RW is supported')
+
+    len_header_fixed_part = 82
+    len_header_radar_locations = len(
+        '<' + ','.join(metadata['radarlocations']) + '> ')
+
+    len_header = len_header_fixed_part + len_header_radar_locations
+
+    # Generate empty header with only whitespaces
+    header_out = np.array(['', ] * len_header, dtype='S1')
+    header_out[:] = ' '
+
+    # Fill header with metadata and tokens
+    header_out[0:2] = list(metadata['producttype'])
+    header_out[2:8] = list(
+        datetime.datetime.strftime(metadata['datetime'], '%d%H%M'))
+    header_out[8:13] = list(metadata['radarid'])
+    header_out[13:17] = list(
+        datetime.datetime.strftime(metadata['datetime'], '%m%y'))
+
+    header_out[17:19] = list('BY')
+    # Have to add one here to get correct length in header string.
+    # Do not know why. Maybe because of the 'etx' char
+    header_out[19:26] = list(str(metadata['datasize'] + len_header + 1))
+
+    header_out[26:28] = list('VS')
+    header_out[28:30] = list(
+        {
+            '100 km and 128 km (mixed)': ' 0',
+            '100 km': ' 1',
+            '128 km': ' 2',
+            '150 km': ' 3'
+        }.get(metadata['maxrange'])
+    )
+
+    header_out[30:32] = list('SW')
+    header_out[32:41] = list(metadata['radolanversion'].rjust(9))
+
+    header_out[41:43] = list('PR')
+    header_out[43:48] = list(
+        {
+            0.01: ' E-02',
+            0.1: ' E-01',
+            1: ' E-00',
+        }.get(metadata['precision'])
+    )
+
+    header_out[48:51] = list('INT')
+    header_out[51:55] = list(
+        str(int(metadata['intervalseconds'] / 60)).rjust(4))
+
+    header_out[55:57] = list('GP')
+    header_out[57:66] = list(
+        str(metadata['nrow']).rjust(4) + 'x' + str(metadata['ncol']).rjust(4))
+
+    header_out[66:68] = list('MF')
+    header_out[69:77] = list(str(int(metadata['moduleflag'])).zfill(8))
+
+    header_out[77:79] = list('MS')
+    header_out[79:82] = list(str(int(len_header_radar_locations)).rjust(3))
+
+    header_out[82:(82 + len_header_radar_locations)] = list(
+        '<' + ','.join(metadata['radarlocations']) + '> ')
+
+    header_out = b''.join(header_out).decode()
+
+    return header_out
